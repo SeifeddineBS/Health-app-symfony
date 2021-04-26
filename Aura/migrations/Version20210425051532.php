d2400ec2aac179dfece026863d39577869ecbc45
Seifeddine Ben Salah <49524346+SeifeddineBS@users.noreply.github.com>
2021-04-26 06:10:56 +0100

    Add files via upload

---
+++ Aura/migrations/Version20210425051532.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210425051532 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD notify_add_coach VARCHAR(255) DEFAULT \'N\' NOT NULL, CHANGE roles roles JSON NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP notify_add_coach, CHANGE roles roles JSON DEFAULT NULL');
+    }
+}
