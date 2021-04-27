e729320d94be33acb8a7ad73c6612d3aa0f3db44
nourdekhil <47943718+nourdekhil@users.noreply.github.com>
2021-04-27 09:42:15 +0200

    Add files via upload

---
+++ Form/EntraideType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Entraide;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EntraideType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('question')
+            ->add('categorie')
+            ->add('date')
+            ->add('email')
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Entraide::class,
+        ]);
+    }
+}
