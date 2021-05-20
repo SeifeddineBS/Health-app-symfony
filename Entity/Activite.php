3b92c16ad66302659d7fa601ee94844bf9d7f6b6
Seifeddine Ben Salah <49524346+SeifeddineBS@users.noreply.github.com>
2021-05-20 01:10:28 +0100

    Add files via upload

--- Entity/Activite.php
+++ Entity/Activite.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Entity;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -14,171 +18,228 @@ class Activite
 {
     /**
      * @var int
-     *
+     *@Groups("post:read")
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     *
      */
     private $id;
 
     /**
      * @var string
-     *
+     *@Groups("post:read")
      * @ORM\Column(name="duree", type="string", length=255, nullable=false)
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 4,
+     *     minMessage = "Duree doit etre minimun 10 min",
+     *      allowEmptyString = false
+     *      )
      */
     private $duree;
 
     /**
-     * @var string
      *
+     * @Assert\Date
+     *@Groups("post:read")
+     * @var date A "d/m/Y" formatted value
+     * @Assert\GreaterThanOrEqual("2021-04-28")
+     *@Assert\NotBlank(message="le champs date est obligatoire  ")
      * @ORM\Column(name="date", type="string", length=255, nullable=false)
      */
     private $date;
 
     /**
      * @var int
-     *
+     * @Groups("post:read")
+     *@Assert\NotBlank(message="le champs nombre max est obligatoire  ")
      * @ORM\Column(name="nombremax", type="integer", nullable=false)
      */
     private $nombremax;
 
     /**
      * @var string
-     *
+     * @Groups("post:read")
+     *@Assert\NotBlank(message="le champs type est obligatoire * ")
      * @ORM\Column(name="type", type="string", length=255, nullable=false)
      */
     private $type;
 
     /**
      * @var string
-     *
+     * @Groups("post:read")
+     *@Assert\NotBlank(message="le champs description est obligatoire  ")
      * @ORM\Column(name="description", type="string", length=255, nullable=false)
      */
     private $description;
 
     /**
      * @var string
-     *
+     * @Groups("post:read")
+     *     *@Assert\NotBlank(message="le champs lieu est obligatoire  ")
      * @ORM\Column(name="lieu", type="string", length=255, nullable=false)
      */
     private $lieu;
 
     /**
      * @var int
-     *
+     * @Groups("post:read")
      * @ORM\Column(name="nombre_parti", type="integer", nullable=false)
      */
     private $nombreParti = '0';
 
     /**
      * @var \User
-     *
-     * @ORM\ManyToOne(targetEntity="User")
+     *@Groups("post:read")
+     * @ORM\ManyToOne(targetEntity=User::class)
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="idcoach", referencedColumnName="id")
      * })
      */
     private $idcoach;
 
+    /**
+     * @return int
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
     public function getDuree(): ?string
     {
         return $this->duree;
     }
 
-    public function setDuree(string $duree): self
+    /**
+     * @param string $duree
+     */
+    public function setDuree(string $duree): void
     {
         $this->duree = $duree;
-
-        return $this;
     }
 
+    /**
+     * @return date
+     */
     public function getDate(): ?string
     {
         return $this->date;
     }
 
-    public function setDate(string $date): self
+    /**
+     * @param date $date
+     */
+    public function setDate(string $date): void
     {
         $this->date = $date;
-
-        return $this;
     }
 
+    /**
+     * @return int
+     */
     public function getNombremax(): ?int
     {
         return $this->nombremax;
     }
 
-    public function setNombremax(int $nombremax): self
+    /**
+     * @param int $nombremax
+     */
+    public function setNombremax(int $nombremax): void
     {
         $this->nombremax = $nombremax;
-
-        return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getType(): ?string
     {
         return $this->type;
     }
 
-    public function setType(string $type): self
+    /**
+     * @param string $type
+     */
+    public function setType(string $type): void
     {
         $this->type = $type;
-
-        return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
     {
         $this->description = $description;
-
-        return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getLieu(): ?string
     {
         return $this->lieu;
     }
 
-    public function setLieu(string $lieu): self
+    /**
+     * @param string $lieu
+     */
+    public function setLieu(string $lieu): void
     {
         $this->lieu = $lieu;
-
-        return $this;
     }
 
+    /**
+     * @return int
+     */
     public function getNombreParti(): ?int
     {
         return $this->nombreParti;
     }
 
-    public function setNombreParti(int $nombreParti): self
+    /**
+     * @param int $nombreParti
+     */
+    public function setNombreParti($nombreParti): void
     {
         $this->nombreParti = $nombreParti;
-
-        return $this;
     }
 
+    /**
+     * @return User
+     */
     public function getIdcoach(): ?User
     {
         return $this->idcoach;
     }
 
-    public function setIdcoach(?User $idcoach): self
+    /**
+     * @param User $idcoach
+     */
+    public function setIdcoach(User $idcoach): void
     {
         $this->idcoach = $idcoach;
-
-        return $this;
     }
 
 
